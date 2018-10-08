e277d1fbab5616837073d671ec2bf31f457143d0
bouzuya <m@bouzuya.net>
2018-10-08 23:26:12 +0900

    add simple json tests

--- test/Main.purs
+++ test/Main.purs
@@ -2,13 +2,40 @@ module Test.Main
   ( main
   ) where
 
+import Data.Either (Either(..), fromRight, isLeft, isRight)
+import Data.List.Lazy.Types (NonEmptyList(..))
+import Data.Maybe (Maybe(..))
+import Data.Nullable (Nullable, toNullable)
 import Effect (Effect)
-import Prelude (Unit)
-import Test.Unit (test)
+import Foreign (MultipleErrors)
+import Partial.Unsafe (unsafePartial)
+import Prelude (Unit, discard)
+import Simple.JSON (readJSON, writeJSON)
+import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
+
+type MaybeJson = { a :: Maybe String }
+type NullableJson = { a :: Nullable String }
 
 main :: Effect Unit
 main = runTest do
   test "Test.Main" do
     Assert.assert "Hello" true
+  suite "Simple.JSON" do
+    test "readJSON (Maybe a)" do
+      let f s = readJSON s :: Either MultipleErrors MaybeJson
+      Assert.equal (Right { a: Just "b" }) (f """{"a":"b"}""")
+      Assert.equal (Right { a: Nothing }) (f """{"a":null}""")
+      Assert.equal (Right { a: Nothing }) (f """{}""")
+    test "readJSON (Nullable a)" do
+      let f s = readJSON s :: Either MultipleErrors NullableJson
+      Assert.equal (Right { a: toNullable (Just "b") }) (f """{"a":"b"}""")
+      Assert.equal (Right { a: toNullable Nothing }) (f """{"a":null}""")
+      Assert.equal true (isLeft (f """{}"""))
+    test "writeJSON (Maybe a)" do
+      Assert.equal """{"a":"b"}""" (writeJSON ({ a: Just "b" } :: MaybeJson))
+      Assert.equal """{}""" (writeJSON ({ a: Nothing } :: MaybeJson))
+    test "writeJSON (Nullable a)" do
+      Assert.equal """{"a":"b"}""" (writeJSON ({ a: toNullable (Just "b") } :: NullableJson))
+      Assert.equal """{"a":null}""" (writeJSON ({ a: toNullable Nothing } :: NullableJson))
